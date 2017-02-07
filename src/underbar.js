64fe036f4691fa180076d8e5af0a8a56ac7f11c2
Paul Mills <paulmills@tests-MacBook-Pro-2.local>
2017-02-07 10:58:14 -0800

    Pass _.map and _.pluck tests
    Write _.map function

--- src/underbar.js
+++ src/underbar.js
@@ -121,6 +121,11 @@
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+    var results = [];
+    _.each(collection, function(element) {
+      results.push(iterator(element));
+    });
+    return results;
   };
 
   /*
